d1acd0e54834c9246477983463cf7f5e9cbb6daa
Arun cs <aruncs31ss@gmail.com>
2025-03-24 10:41:25 +0530

    update pi

--- src/model_run.py
+++ src/model_run.py
@@ -1,19 +1,38 @@
+# NOTE: Should run only on raspberry pi
 import time
-
+from RPLCD.i2c import CharLCD
 import cv2  # python-opencv
-import numpy as np
+import numpy as np 
 import yaml
 from ultralytics import YOLO
 
-# NOTE: This is the trained model
-# TODO: checkout how to train multiple dataset and make it a single model?
-model = YOLO("src/yolo11n.onnx", task="detect")
+# sudo i2cdetect -y 1
+LCD_ADDR = 0x27
 
-with open("src/properties.yaml", "r") as properties_file:
-    properties = yaml.safe_load(properties_file)
+class LCD:
+    def __init__(self,ADDR):
+        self.lcd = CharLCD('PCF8574', ADDR, cols=16, rows=2)
+        # Clear the LCD
+        self.lcd.clear()
+        self.lcd.write_string(u"Object Detection")
+        time.sleep(5)
+        # Move to the second line
+        self.lcd.crlf()
+
+    def display(self,status,class_name):
+        # TODO: Check if this is neccessary
+        self.lcd.clear()
+        # self.lcd.cursor_pos = (1, 0)
+        if status:
+            self.lcd.write_string("Object Detected")
+            self.lcd.crlf()
+            self.lcd.write_string(u" {class_name}")
+        else:
+            self.lcd.write_string("No Object Detected")
+
+        
 
 
-# used to detect and draw the bounding box
 def detect_objects(frame):
     results = model(frame)
     for result in results:
@@ -33,29 +52,38 @@ def detect_objects(frame):
     return annotated_frame
 
 
-# Start video capture from USB camera
-video_capture = cv2.VideoCapture(0)
+def main():
+    newLCD = LCD(LCD_ADDR)
+    model = YOLO("src/yolo11n.onnx", task="detect")
+    with open("src/properties.yaml", "r") as properties_file:
+        properties = yaml.safe_load(properties_file)
 
-if not video_capture.isOpened():
-    print("Error: Could not open video.")
-    exit()
+    video_capture = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = video_capture.read()
-    if not ret:
-        print("Error: Could not read frame.")
-        break
+    if not video_capture.isOpened():
+        print("Video error")
+        exit()
 
-    detected_frame = detect_objects(frame)
+    while True:
+        ret, frame = video_capture.read()
+        if not ret:
+            print("Error: Could not read frame.")
+            break
 
-    if properties["SHOW_CLASS_NAME"]:
-        cv2.imshow("Detected PPE Objects", detected_frame)
-        print(f"Object detected")
+        detected_frame = detect_objects(frame)
 
-    time.sleep(0.03)  # Adjust to control the speed
+        if properties["SHOW_CLASS_NAME"]:
+            cv2.imshow("Detected PPE Objects", detected_frame)
+            print(f"Object detected")
+            newLCD.display(True,)
+        time.sleep(0.03)  # Adjust to control the speed
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
-video_capture.release()
-cv2.destroyAllWindows()
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
